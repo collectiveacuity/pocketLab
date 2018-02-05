76590547bbb04c454bf4f30fbcf340440b4be764
/home/rcj1492 <richard@collectiveacuity.com>
2018-02-05 21:01:41 +0000

    added stall docker image and container removal to clean command; TODO: add docker-compose.yml to lab init; TODO add lab start to run docker containers based upon docker-compose.yml;

--- pocketlab/commands/clean.py
+++ pocketlab/commands/clean.py
@@ -15,8 +15,16 @@ _clean_details = {
     'benefit': 'Frees up space by removing superfluous files.'
 }
 
-def clean(verbose=True):
+def clean(verbose=True, virtualbox='default'):
 
+    '''
+        a method to remove orphaned resources in lab repositories
+        
+    :param verbose: [optional] boolean to toggle process messages
+    :param virtualbox: [optional] string with name of virtualbox image (win7/8)
+    :return: string with exit message
+    '''
+    
 # construct registry client
     from pocketlab import __module__
     from labpack.storage.appdata import appdataClient
@@ -42,9 +50,39 @@ def clean(verbose=True):
                 print('Broken service "%s" removed from lab registry.' % file_name)
             remove_settings(file_path)
 
-# TODO remove docker containers with exit 1 status
+# construct docker client
+    from labpack.platforms.docker import dockerClient
+    try:
+        docker_client = dockerClient(virtualbox_name=virtualbox)
+    except:
+        docker_client = None
 
-# TODO remove docker images with ^none name
+# remove docker containers with exit 1 status
+    if docker_client:
+        for container in docker_client.ps():
+            if container['STATUS'].find('Exited (1)') > -1:
+                try:
+                    docker_client.rm(container['CONTAINER_ID'])
+                    if verbose:
+                        if container['NAMES']:
+                            container_name = 'alias "%s"' % container['NAMES'].split(' ')[0]
+                        else:
+                            container_name = 'of image "%s"' % container['IMAGE']
+                        print('Container %s with exit(1) status removed from docker.' % container_name)
+                except:
+                    pass
+
+# remove docker images with <none> in name tag
+    if docker_client:
+        for image in docker_client.images():
+            if image['TAG'] == '<none>':
+                try:
+                    docker_client.rmi(image['IMAGE ID'], override=True)
+                    if verbose:
+                        image_name = '"%s:%s"' % (image['REPOSITORY'], image['TAG'])
+                        print('Image %s removed from docker.' % image_name)
+                except:
+                    pass
 
     exit_msg = 'Lab environment has been cleaned up.'
 
